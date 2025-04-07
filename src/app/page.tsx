c5ec91d439ee9c2a9925919b9a8606cc061d84c5
aayman01 <ayman.alfi098@gmail.com>
2025-04-07 15:52:56 +0600

    material ui added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 
 
 export default function Home() {
   return (
     <div>
       <h2>this is the home page</h2>
+      <Button variant="contained">Text</Button>
     </div>
   );
 }
