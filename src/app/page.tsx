fad06d5984a8443b07544c25697ec137e20712f3
aayman01 <ayman.alfi098@gmail.com>
2025-04-08 16:55:07 +0600

    image upload functionality complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+"use client";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ImageUpload from "@/components/ImageUpload";
 import { Container } from "@mui/material";
 
-
 export default function Home() {
+  const fetchImages = async () => {
+    console.log("Fetching images...");
+  };
+
   return (
     <>
-      <Header/>
+      <Header />
       <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
-        <ImageUpload />
+        <ImageUpload onUpload={fetchImages} />
       </Container>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -9,15 +9,44 @@ import {
   Alert,
 } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { supabase } from "../lib/supabaseClient";
 
-const ImageUpload = () => {
+
+const ImageUpload = ({ onUpload }: { onUpload: () => void }) => {
   const [isUploading, setIsUploading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState(false);
 
-  const handleUpload = async (e : React.ChangeEvent<HTMLInputElement>) =>{
-    console.log(e.target.files);
-  }
+  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.files);
+    setError(null);
+    setSuccess(false);
+    const files = e.target.files;
+    if (!files) return setError("No files selected.");
+
+    try {
+      setIsUploading(true);
+      for (const file of Array.from(files)) {
+        const { data, error } = await supabase.storage
+          .from("aayman")
+          .upload(file.name, file);
+        if (error) console.error("Upload error:", error);
+        console.log(data);
+      }
+      
+      onUpload();
+    } catch (error) {
+      setError(
+        error instanceof Error ? error.message : "An unknown error occurred"
+      );
+    } finally {
+      setIsUploading(false);
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 3000); // Hide success message after 3 seconds
+    }
+  };
 
   return (
     <Box sx={{ mb: 4, textAlign: "center" }}>
