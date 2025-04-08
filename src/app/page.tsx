e7e74842ccbb749742c1940bc6c934d24887b0df
aayman01 <ayman.alfi098@gmail.com>
2025-04-08 12:17:56 +0600

    header created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@mui/material";
+import Header from "@/components/Header";
+
 
 
 export default function Home() {
   return (
-    <div>
-      <h2>this is the home page</h2>
-      <Button variant="contained">Text</Button>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import CollectionsIcon from "@mui/icons-material/Collections";
+import Box from "@mui/material/Box";
+
+export default function Header() {
+    // const theme = useTheme();
+    // const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Box sx={{ display: "flex", alignItems: "center", mr: 2 }}>
+          <CollectionsIcon sx={{ mr: 1 }} />
+          <Typography
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1}}
+          >
+            Image Gallery
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
